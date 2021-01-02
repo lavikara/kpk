7a0ccbde4aa25994631379f32481e4418f30502d
lavikara <karatemitayo@gmail.com>
2021-01-02 19:23:16 +0100

    feat: setup express server

---
+++ src/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const env = require("./config/env.js");
+const cors = require("cors");
+const connectDB = require("../db/connection.js");
+const logger = require("./middlewares/logger.js");
+const bodyparser = require("body-parser");
+
+const app = express();
+
+const userRouter = require("./routes/user.js");
+const productRouter = require("./routes/product.js");
+const cartRouter = require("./routes/cart.js");
+
+connectDB();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(logger);
+app.use(cors());
+
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/cart", cartRouter);
+
+app.listen(env.config.PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(` 🛒 KPK 🛒  I'm alive on ${env.config.PORT} 🗼 🗼 `);
+  }
+});
