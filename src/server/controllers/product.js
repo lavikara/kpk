e0d5f70f09cb2726b5afa2f8419443fbc4bbc275
lavikara <karatemitayo@gmail.com>
2021-01-02 19:26:06 +0100

    feat: created get single product, and create product methods

---
+++ src/server/controllers/product.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const env = require("../config/env.js");
+const productmodel = require("../../db/models/productModel.js");
+
+exports.create_product = () => {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const tokendata = jwt.verify(token, env.config.JWT_SECRET);
+      req.body.vendor_id = tokendata.id;
+      req.body.quantity = 0;
+      req.body.sub_total = 0;
+      const product = await productmodel.create(req.body);
+
+      res.status(200).send({
+        status: "success",
+        message: "product was successfully created",
+        data: product,
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).send({
+        status: "error",
+        message: "an error occured while creating your product",
+      });
+    }
+  };
+};
+
+// used in add to cart method
+exports.get_single_product = async (id) => {
+  try {
+    return await productmodel.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+};
