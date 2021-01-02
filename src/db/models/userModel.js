bacbc87b538f62c64da8879c963a776c8dd9db80
lavikara <karatemitayo@gmail.com>
2021-01-02 19:22:43 +0100

    feat: created user schema

---
+++ src/db/models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+/* mongoose user schema */
+
+const userschema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: Object,
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  business_name: {
+    type: String,
+  },
+  cac_number: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "vendor"],
+    required: true,
+  },
+});
+
+const usermodel = mongoose.model("users", userschema);
+
+module.exports = usermodel;
