ad7c9ed8275af9831112cfbe04f9edefbab9273b
lavikara <karatemitayo@gmail.com>
2021-01-09 04:16:58 +0100

    removed get cart logic on login

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -73,20 +73,11 @@ exports.login_user = () => {
       delete userobj.password;
       delete userobj._id;
 
-      if (userobj.role === "customer") {
-        const userCart = await cart.get_cart_on_login(userobj.id);
-        const token = jwt.sign({ id: userobj.id }, env.config.JWT_SECRET);
-        res.status(200).send({
-          status: "success",
-          data: { user: userobj, token, userCart },
-        });
-      } else {
-        const token = jwt.sign({ id: userobj.id }, env.config.JWT_SECRET);
-        res.status(200).send({
-          status: "success",
-          data: { user: userobj, token },
-        });
-      }
+      const token = jwt.sign({ id: userobj.id }, env.config.JWT_SECRET);
+      res.status(200).send({
+        status: "success",
+        data: { user: userobj, token },
+      });
     } catch (err) {
       console.log(err);
       res.status(500).send({
