401e626989b8cb35a19b3236dbd7d6dca6674605
lavikara <karatemitayo@gmail.com>
2021-01-13 19:16:55 +0100

    added endpoints to create subacounts and get bank list

--- src/server/controllers/payment.js
+++ src/server/controllers/payment.js
@@ -1,4 +1,7 @@
 const api = require("../utils/api.js");
+const jwt = require("jsonwebtoken");
+const env = require("../config/env.js");
+const usermodel = require("../../db/models/userModel.js");
 
 exports.pay_with_flutter = () => {
   return async (req, res, next) => {
@@ -48,6 +51,102 @@ exports.verify_payment = (id) => {
       res.status(500).send({
         status: "error",
         message: "an error occured while making payment",
+      });
+    }
+  };
+};
+
+exports.get_bank_list = () => {
+  return async (req, res, next) => {
+    try {
+      await api
+        .getBankList(req.body.country)
+        .then(({ data }) => {
+          res.status(200).send({
+            status: "success",
+            message: "Fetched bank list",
+            data,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).send({
+        status: "error",
+        message: "an error occured while getting bank list",
+      });
+    }
+  };
+};
+
+exports.create_rider_sub_account = () => {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const tokendata = jwt.verify(token, env.config.JWT_SECRET);
+      req.body.split_type = "percentage";
+      req.body.split_value = 0.8;
+      const accountDetails = await api
+        .createSubAccount(req.body)
+        .then(({ data }) => {
+          return data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      await usermodel.findOneAndUpdate(
+        { _id: tokendata.id },
+        { account_details: accountDetails, rider_status: true },
+        { new: true }
+      );
+      res.status(200).send({
+        status: "success",
+        message: "Account created",
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).send({
+        status: "error",
+        message: "an error occured while creating sub-account",
+      });
+    }
+  };
+};
+
+exports.create_vendor_sub_account = () => {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const tokendata = jwt.verify(token, env.config.JWT_SECRET);
+      req.body.split_type = "percentage";
+      req.body.split_value = 0.975;
+      const accountDetails = await api
+        .createSubAccount(req.body)
+        .then(({ data }) => {
+          return data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      await usermodel.findOneAndUpdate(
+        { _id: tokendata.id },
+        { account_details: accountDetails },
+        { new: true }
+      );
+      res.status(200).send({
+        status: "success",
+        message: "Account created",
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).send({
+        status: "error",
+        message: "an error occured while creating sub-account",
       });
     }
   };
--- src/server/routes/payment.js
+++ src/server/routes/payment.js
@@ -17,5 +17,22 @@ router.get(
   // validator(joischema.createProduct),
   payment.verify_payment()
 );
+router.post(
+  "/bank-list",
+  // validator(joischema.createProduct),
+  payment.get_bank_list()
+);
+router.post(
+  "/rider-sub-account",
+  authorization(),
+  // validator(joischema.createProduct),
+  payment.create_rider_sub_account()
+);
+router.post(
+  "/vendor-sub-account",
+  authorization(),
+  // validator(joischema.createProduct),
+  payment.create_vendor_sub_account()
+);
 
 module.exports = router;
