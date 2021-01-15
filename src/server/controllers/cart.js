4eb4a2d65a8bde32a3b2373bce25f5ae7a21d58a
lavikara <karatemitayo@gmail.com>
2021-01-15 14:50:21 +0100

    adjusted dispatch amount calculation

--- src/server/controllers/cart.js
+++ src/server/controllers/cart.js
@@ -138,7 +138,7 @@ exports.remove_from_cart = () => {
         (accumulator, currentValue) => accumulator + currentValue.quantity,
         initialValue
       );
-      cart.dispatch -= 5;
+      cart.dispatch = 5 * cart.total_quantity;
 
       const updatedCart = await cartmodel.findOneAndUpdate(
         { _id: tokendata.id },
