f167cc8422a9c595de0150893b0d908009bcfc8f
lavikara <karatemitayo@gmail.com>
2021-01-19 07:08:13 +0100

    fixed order history bug

--- src/server/controllers/hook.js
+++ src/server/controllers/hook.js
@@ -59,7 +59,7 @@ exports.flutter_hook = () => {
             console.log("order: ", customerHistory);
             await historyModal.findOneAndUpdate(
               { _id: userId },
-              { order_history: order }
+              { order_history: customerHistory }
             );
             await cartmodel.findOneAndUpdate(
               { _id: userId },
