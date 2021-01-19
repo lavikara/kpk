5c9c55317f1b4a367d92a8317d37e0096de27b71
lavikara <karatemitayo@gmail.com>
2021-01-19 18:00:09 +0100

    added endpoint to get customer order history

--- src/server/controllers/history.js
+++ src/server/controllers/history.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const env = require("../config/env.js");
 const historyModal = require("../../db/models/historyModal.js");
 
 exports.order_history = async (id) => {
@@ -9,4 +11,26 @@ exports.order_history = async (id) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+exports.get_customer_history = () => {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const tokendata = jwt.verify(token, env.config.JWT_SECRET);
+      const history = await historyModal.findById(tokendata.id);
+
+      res.status(200).send({
+        status: "success",
+        data: { history },
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).send({
+        status: "error",
+        message: "an error occured while getting customer cart",
+      });
+    }
+  };
 };
